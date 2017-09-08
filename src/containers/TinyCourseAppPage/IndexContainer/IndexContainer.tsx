26983e0bc5b97a8f4e1b01a2b6d6770722c4e3c1
zhanheng1993 <heng.zhan@ichangtou.com>
2017-09-08 11:24:53 +0800

    2017.9.8index

--- src/containers/TinyCourseAppPage/IndexContainer/IndexContainer.tsx
+++ src/containers/TinyCourseAppPage/IndexContainer/IndexContainer.tsx
@@ -57,7 +57,11 @@ class IndexContainer extends React.Component<PropsTypes, StateTypes> {
     render() {
         return (
             <div className={className.indexPage}>
-                <div style={{paddingLeft: 0, paddingRight: 0, paddingBottom: '16px'}}>
+                <div className={className.bannerWrap} style={{paddingLeft: 0, paddingRight: 0, paddingBottom: '16px'}}>
+                    <div className={className.bannerText}>
+                        <p className={className.bannerTextTop}>{this.props.DALIndexPageState.title.split('#')[0]}</p>
+                        <p>{this.props.DALIndexPageState.title.split('#')[1] || ''}</p>
+                    </div>
                     <ImageCard src={this.props.DALIndexPageState.cover}></ImageCard>
                 </div>
                 <div className={className.indexPageInner}>
@@ -95,18 +99,16 @@ class IndexContainer extends React.Component<PropsTypes, StateTypes> {
 }
 IndexContainer.defaultProps = {
     DALIndexPageState: {
-        cover: 'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png',
-        price: 1,
-        intro: '学理财来长投学理财来长投学理财来长投学理财来长投#学理财来长投学理财来长投学理财来长投',
-        outline: ['啊啊啊', '啊啊啊'],
-        audioSrc: 'sdsadsada',
-        hasFetchData: true,
-        teacher: {
-            avatar: 'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png',
-            intro: 'sdasdasjdhajkdhaskjdkajdsd',
-            name: 'jksadhjaksjdhaksjdhkasjdaskj'
-        }
-    },
+        "id":1,
+        "title":"五本书#带你进入理财大门",
+        "cover":"https://source.ichangtou.com/file/tinyCourse/pic/1-0.png",
+        "price":1,"audio":"https://source.ichangtou.com/file/tinyCourse/audio/1-0.mp3",
+        "outline":"《富爸爸穷爸爸》--财富启蒙之路#《财富自由之路》--没你想得那么难#《邻家的百万富翁》--你认识的都是假的富人？#《贫穷的本质》--寒门再难出贵子？#《牛奶可乐经济学》--最有趣的经济学","intro":"很多小伙伴都觉得，投资书籍一定是艰深晦涩的，没读几行就打瞌睡；也有人觉得，很多理财投资畅销书，是不是跟很多成功学、励志类图书一样，都是洗脑骗人的！其实，与其总是“自己觉得”、“自己猜想”，不妨从理解和认识开始。\r\r这门小课，给你介绍五本世界经典的理财书籍，观点颠覆三观，而且常读常新。读了这5本书，你会对“理财”这个领域，有一个全新的理解。\r","position":"https://source.ichangtou.com/file/tinyCourse/pic/1-position.png",
+        "teacher":{
+            "avatar":"木有",
+            "intro":"90后逗逼股票分析师；最会量化投资的程序员；著有电子畅销书《给投资新手的极简股票课》，亚马逊发布首周，牢牢占据投资理财类畅销商品No.1",
+            "name":"Lip师兄"
+        }},
     propsPath: '',
     history: []
 }
