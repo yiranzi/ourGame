7fa38f36a450cfe084f9e570c5af812adee805fa
yiranzi <570932296@qq.com>
2017-09-19 09:23:36 +0800

    commit set

--- src/main2.js
+++ src/main2.js
@@ -13,6 +13,11 @@ var gameOptions = {
     // number of tiles visible, works better if it's even, in this first prototype
     verticalTiles: 9
 }
+
+var babyGameOptions = {
+    verticalTiles: 9,
+    widthTiles: 3,
+}
 window.onload = function() {
 
     // determining window width and height
@@ -26,6 +31,7 @@ window.onload = function() {
 
     // defining game height
     var gameHeight = windowHeight * gameOptions.gameWidth / windowWidth;
+    // var gameHeight = windowHeight * gameOptions.gameWidth / windowWidth;
 
     // creation of the game istelf
     game = new Phaser.Game(gameOptions.gameWidth, gameHeight);
@@ -53,7 +59,121 @@ preloadGame.prototype = {
 }
 var playGame = function(game){}
 playGame.prototype = {
+    create: function () {
+        //map
+        this.tileSize = 100;
+
+        //
+        this.moveStep = 0;
+
+        let offsetX = (game.width - this.tileSize * babyGameOptions.widthTiles) / 2;
+        let offsetY = (game.height - this.tileSize * babyGameOptions.verticalTiles) / 2;
+        let _this = this;
+
+        // amount of placed tiles, useful to tint even/odd tiles with different colors
+        var placedTiles = 0;
+        //sun
+
+        //居中
+        // horizontal offset to keep tiles centered in the game
+        // var offsetX = (game.width - this.tileSize * 3) / 2;
+
+        //group which will contain all tiles
+        this.tileGroup = game.add.group();
+        this.tileGroup.x = offsetX
+        this.tileGroup.y = 0
+        this.tileGroupTween = game.add.tween(this.tileGroup).to({
+            y: this.tileSize
+        }, 100, Phaser.Easing.Linear.None);
+
+        this.tileArray = [];
+        for( let i = 0; i < babyGameOptions.widthTiles; i++) {
+            this.tileArray[i] = [];
+            for( let j = 0; j < babyGameOptions.verticalTiles; j++) {
+                // console.log(i + '/' + j)
+                let tile = game.add.sprite(i * this.tileSize,j * this.tileSize,"tile");
+                tile.width = this.tileSize
+                tile.height = this.tileSize
+                this.tileGroup.add(tile)
+                tile.anchor.set(0, 1);
+                this.tileArray[i][j] = tile;
+            }
+
+        }
+
+        this.tileGroupTween.onComplete.add(function () {
+            _this.tileGroup.y = 0;
+            console.log(_this.tileGroup.y)
+            console.log(_this.myHero.y)
+            //y 加上去
+            _this.tileGroup.forEach(function (child) {
+                child.y += _this.tileSize;
+            }, _this);
+            let step = _this.moveStep % babyGameOptions.verticalTiles;
+            for( let i = 0; i< babyGameOptions.widthTiles; i++) {
+                _this.tileArray[i][babyGameOptions.verticalTiles -1 - step].y -=  babyGameOptions.verticalTiles * _this.tileSize
+            }
+            _this.moveStep++;
+
+        })
+
+
+
+
+        let startButton = game.add.button(0,0,"tile",function () {
+            _this.tileGroupTween.start();
+        });
+
+
+
+
+        this.pos = {
+            x: 1,
+            y: 2,
+        }
+        this.canMove = true;
+        this.myHero = game.add.sprite(this.tileGroup.x + this.getPos(this.pos.x),this.getPos(this.pos.y),"hero");
+        this.myHero.width  = this.tileSize;
+        this.myHero.height  = this.tileSize;
+        this.getPos();
+        // this.tileGroup.add(this.myHero);
+
+        // // this.hero = game.add.sprite(0,0,"hero")
+        // let _this = this;
+        // this.heroMoveTween = game.add.tween(this.hero);
+        // game.input.onDown.add(this.moveMyHero, this);
+        //
+        // this.heroMoveTween.onComplete.add(function () {
+        //     _this.canMove = true;
+        //     console.log('123');
+        //     console.log(_this.pos.x)
+        // })
+
+    },
+    getPos(pos) {
+        return pos * this.tileSize;
+    },
+
+    moveMyHero: function (e) {
+        if(!this.canMove) {
+            return
+        }
+        this.canMove = false;
+        this.heroMoveTween.timeline = [];
+        this.pos.x = e.position.x;
+        this.pos.y = e.position.y;
+        this.heroMoveTween.to({
+            x: this.pos.x,
+            y:this.pos.y,
+        },500,Phaser.Easing.Linear.None);
+        this.heroMoveTween.start();
+
+    }
+
+}
+playGame.prototypeTest = {
     create: function(){
+        console.log('123')
 
         // useful to count travelled distance
         //计算距离的变量
@@ -78,17 +198,17 @@ playGame.prototype = {
 
         // placing the group to have tiles centered in the game
         this.tileGroup.x = offsetX;
+        this.tileGroup.y = 0;
 
         // creation of a tween which will scroll the terrain down by one tile
         this.tileTween = game.add.tween(this.tileGroup).to({
             y: this.tileSize
-        }, 100, Phaser.Easing.Linear.None);
+        }, 1000, Phaser.Easing.Linear.None);
 
         // since the endless runner thing is a fake, once we moved the terrain down by a tile
         // we reset its position, then move the lowest tiles to the top, giving the idea of an
         // infinite terrain
         this.tileTween.onComplete.add(function(){
-            console.log("tileTween.onComplete")
             this.tileGroup.y = 0;
             // this.tileGroup.forEach(function(child){
             //     child.y += this.tileSize;
@@ -96,7 +216,7 @@ playGame.prototype = {
             // for(var i = 0; i < 3; i++){
             //     this.tileArray[this.moves % this.tileArray.length][i].y -= (gameOptions.verticalTiles + 1) * this.tileSize
             // }
-            this.moves ++;
+            // this.moves ++;
         }, this);
 
         // placing and tinting terrain tiles
@@ -132,7 +252,6 @@ playGame.prototype = {
 
         // callback function to be called once the tween is complete
         this.heroTween.onComplete.add(function(){
-            console.log('完成heroTween')
             this.heroCanMove = true;
             this.hero.x = this.tileGroup.x + this.tileSize * this.heroColumn;
             // this.heroWrap.visible = false;
@@ -180,11 +299,10 @@ playGame.prototype = {
             // new hero destination
             this.heroTween.to({
                 x: this.hero.x + this.tileSize * direction
-            }, 100, Phaser.Easing.Cubic.InOut, true);
+            }, 1000, Phaser.Easing.Cubic.InOut, true);
 
             // this is the case with the wrapping hero coming into play
             if(Math.abs(nextColumn - this.heroColumn) != 1){
-                console.log(this.heroColumn)
                 // making it visible
                 this.heroWrap.visible = true;
 
@@ -192,7 +310,7 @@ playGame.prototype = {
                 this.heroWrap.x = nextColumn == 0 ? this.tileGroup.x - this.tileSize: this.tileGroup.x + 3 * this.tileSize;
 
                 // resetting tween timeline
-                this.heroWrapTween.timeline = [];
+                // this.heroWrapTween.timeline = [];
 
                 // finally making the wrap hero move
                 this.heroWrapTween.to({
@@ -202,7 +320,6 @@ playGame.prototype = {
             }
             this.heroColumn = nextColumn;
         } else {
-            console.log('123')
         }
     }
 }
